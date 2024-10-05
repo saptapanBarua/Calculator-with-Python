1f3aca50dce653e92519b1f676b9e8e4c9f6be91
Saptapan Barua <122849840+saptapanBarua@users.noreply.github.com>
2024-10-05 23:15:00 +0600

    implement function

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+def addition(n1, n2):
+    result=n1+n2
+    print(f'{n1}+{n2}={result}')
+
+def subtraction(n1, n2):
+    result=n1-n2
+    print(f'{n1}-{n2}={result}')
+
+def multiplication(n1, n2):
+    result=n1*n2
+    print(f'{n1}*{n2}={result}')
+
+def division(n1, n2):
+    result=n1/n2
+    print(f'{n1}/{n2}={result}')
+
 while(True):
     print('''
 **************************************
@@ -12,15 +28,18 @@ while(True):
         or press Q or q to exit the calculator''')
     choice=input("\nEnter your choice : ")
 
-    num1=float(input("Enter num1 : "))
-    num2=float(input("Enter num2 : "))
-    if choice=='1':
-        addition(num1, num2)
-    elif choice=='2':
-        subtraction(num1, num2)
-    elif choice=='3':
-        multiplication(num1, num2)
-    elif choice=='4':
-        division(num1, num2)    
+    if (choice=='Q' or choice=='q'):
+        break
     else:
-        print('Invalid choice')
+        num1=float(input("Enter num1 : "))
+        num2=float(input("Enter num2 : "))
+        if choice=='1':
+            addition(num1, num2)
+        elif choice=='2':
+            subtraction(num1, num2)
+        elif choice=='3':
+            multiplication(num1, num2)
+        elif choice=='4':
+            division(num1, num2)    
+        else:
+            print('Invalid choice')
